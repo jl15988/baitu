9870ef3ed94503009a66bf8c1e77ff117ae2f190
jl15988 <jl15988@qq.com>
2024-08-26 19:46:47 +0800

    perf: 优化树工具

--- src/modules/common/TreeUtil.ts
+++ src/modules/common/TreeUtil.ts
@@ -1,6 +1,23 @@
 import ArrayUtil from "../array/ArrayUtil";
 import NumberUtil from "../number/NumberUtil";
 
+export type BuildTreeOptionType<T> = {
+    // 主键属性名
+    idName?: keyof T
+    // 父级主键属性名
+    parentName?: keyof T
+    // 叶子节点处理函数
+    leafMapper?: TreeLeafNodeMapper<T>
+}
+
+export type BuildTreeLevelOptionType<T> = BuildTreeOptionType<T> & {
+    // 子节点属性名
+    childName?: keyof T
+    // 节点处理函数
+    mapper?: TreeNodeMapper<T>
+    // 等级属性名
+    levelName?: keyof T
+}
 /**
  * 树节点处理函数
  * @param current 当前元素
@@ -8,74 +25,69 @@ import NumberUtil from "../number/NumberUtil";
  * @param parentChildList 父级子元素集合
  * @param level 当前层级
  */
-export type TreeNodeMapper<T> = (current: T, parent?: T, parentChildList?: T[], level?: number) => T;
+export type TreeNodeMapper<T> = (current: T, parent?: T, parentChildList?: T[], level?: number) => T
 /**
  * 树叶子节点处理函数
  * @param current 当前元素
  */
-export type TreeLeafNodeMapper<T> = (current: T) => T;
-
-type TreeNodeType<C extends string | number | symbol, T> = T & {
-    [P in C]: TreeNodeType<C, T>[]
-}
+export type TreeLeafNodeMapper<T> = (current: T) => T
 
 class TreeUtil {
-
     /**
      * 默认的级别属性名
      */
-    LEVEL_NAME = '__tree_level';
+    LEVEL_NAME = '__tree_level'
     /**
      * 默认的开始级别
      */
-    LEVEL_BEGIN = 1;
+    LEVEL_BEGIN = 1
     /**
      * 已构建标记
      */
-    BUILT_TAG = '__built_tag';
+    BUILT_TAG = '__built_tag'
 
     /**
      * 构建树结构（保留非根节点）
      * @param list 数组
-     * @param idName 主键属性名
-     * @param parentName 父级主键属性名
-     * @param childName 子节点属性名
-     * @param levelName 级别属性名
-     * @param mapper 节点处理函数
-     * @param leafMapper 叶子节点处理函数
+     * @param options 配置信息
      */
-    buildTree<T extends (Record<I, any> & Record<P, any>), I extends string, P extends string, C extends string, R extends TreeNodeType<C, T>>
-    (list: T[], idName: I, parentName: P, childName: C, levelName?: string, mapper?: TreeNodeMapper<T>, leafMapper?: TreeLeafNodeMapper<T>): R[] {
-        if (!list) return [];
-        let newList = ArrayUtil.deepCopy(list);
-        levelName = levelName || this.LEVEL_NAME;
-        this.buildTreeMapper(newList, idName, parentName, (current, parent, parentChildList, level) => {
-            if (level !== this.LEVEL_BEGIN) {
-                // 根节点遍历时没有父级
-                // @ts-ignore
-                parent[childName] = parentChildList;
+    buildTree<T extends Record<string, any>>(list: T[], options?: BuildTreeLevelOptionType<T>): T[] {
+        if (!list) return []
+        const newList = ArrayUtil.deepCopy(list) as T[]
+        const {idName, parentName, mapper, leafMapper} = options || {}
+        let {childName, levelName} = options || {}
+        childName = childName || 'children'
+        levelName = levelName || this.LEVEL_NAME
+        this.buildTreeMapper(
+            newList,
+            (current, parent, parentChildList, level) => {
+                if (level !== this.LEVEL_BEGIN) {
+                    // 根节点遍历时没有父级
+                    parent[childName] = parentChildList as T[keyof T]
+                }
+                current[levelName] = level as T[keyof T]
+                mapper && mapper(current, parent, parentChildList, level)
+                return current
+            },
+            {
+                idName,
+                parentName,
+                leafMapper
             }
-            // @ts-ignore
-            current[levelName] = level;
-            mapper && mapper(current, parent, parentChildList, level);
-            return current;
-        }, leafMapper);
-        return newList;
+        )
+        return newList
     }
 
     /**
      * 构建树结构
      * @param list 数组
-     * @param idName 主键属性名
-     * @param parentName 父级主键属性名
-     * @param childName 子节点属性名
-     * @param levelName 级别属性名
-     * @param mapper 节点处理函数
-     * @param leafMapper 叶子节点处理函数
+     * @param options 配置信息
      */
-    buildCommonTree<T extends (Record<I, any> & Record<P, any>), I extends string, P extends string, C extends string, R extends TreeNodeType<C, T>>
-    (list: T[], idName: I, parentName: P, childName: C, levelName?: string, mapper?: TreeNodeMapper<T>, leafMapper?: TreeLeafNodeMapper<T>): R[] {
-        return this.filterRoot(this.buildTree(list, idName, parentName, childName, levelName, mapper, leafMapper))
+    buildCommonTree<T extends Record<string, any>>(
+        list: T[],
+        options?: BuildTreeLevelOptionType<T>
+    ): T[] {
+        return this.filterRoot(this.buildTree(list, options))
     }
 
     /**
@@ -84,28 +96,29 @@ class TreeUtil {
      * @param levelName 级别属性名
      */
     filterRoot<T>(list: T[], levelName?: string) {
-        levelName = levelName || this.LEVEL_NAME;
+        levelName = levelName || this.LEVEL_NAME
         // @ts-ignore
-        return list.filter(item => item[levelName] === this.LEVEL_BEGIN);
+        return list.filter((item) => item[levelName] === this.LEVEL_BEGIN)
     }
 
     /**
      * 递归树结构变换器
      * @param list 全数组
-     * @param idName 主键属性名
-     * @param parentName 父级主键属性名
      * @param mapper 节点处理函数
-     * @param leafMapper 叶子节点处理函数
+     * @param options 配置信息
      */
-    buildTreeMapper<T extends (Record<I, any> & Record<P, any>), I extends string, P extends string>
-    (list: T[], idName: I, parentName: P, mapper: TreeNodeMapper<T>, leafMapper?: TreeLeafNodeMapper<T>): void {
-        if (ArrayUtil.isEmpty(list)) return;
-        for (let item of list) {
-            const level = this.LEVEL_BEGIN;
-            // @ts-ignore
-            if (item[this.BUILT_TAG]) continue;
-            mapper && mapper(item, undefined, [], level);
-            this.toTreeMapper(list, item, idName, parentName, mapper, leafMapper, level);
+    buildTreeMapper<T extends Record<any, any>>(
+        list: T[],
+        mapper: TreeNodeMapper<T>,
+        options?: BuildTreeOptionType<T>
+    ): void {
+        if (!list || list.length <= 0) return
+        const {idName, parentName, leafMapper} = options || {}
+        for (const item of list) {
+            const level = this.LEVEL_BEGIN
+            if (item[this.BUILT_TAG]) continue
+            mapper && mapper(item, undefined, [], level)
+            this.toTreeMapper(list, item, mapper, idName, parentName, leafMapper, level)
         }
     }
 
@@ -119,25 +132,33 @@ class TreeUtil {
      * @param leafMapper 叶子节点处理函数
      * @param parentLevel 上级层级
      */
-    toTreeMapper<T extends (Record<I, any> & Record<P, any>), I extends string, P extends string, C extends string, R extends TreeNodeType<C, T>>
-    (list: T[], obj: T, idName: I, parentName: P, mapper: TreeNodeMapper<T>, leafMapper?: TreeLeafNodeMapper<T>, parentLevel?: number): void {
-        const childList = [];
-        for (let item of list) {
-            // @ts-ignore
+    toTreeMapper<T extends Record<any, any>>(
+        list: T[],
+        obj: T,
+        mapper: TreeNodeMapper<T>,
+        idName?: keyof T,
+        parentName?: keyof T,
+        leafMapper?: TreeLeafNodeMapper<T>,
+        parentLevel?: number
+    ): void {
+        idName = idName || 'id'
+        parentName = parentName || 'parentId'
+        const childList: T[] = []
+        for (const item of list) {
             if (item[parentName] === obj[idName]) {
                 // @ts-ignore
-                item[this.BUILT_TAG] = true;
-                childList.push(item);
+                item[this.BUILT_TAG] = true
+                childList.push(item)
             }
         }
         if (childList.length > 0) {
-            for (let children of childList) {
+            for (const children of childList) {
                 const level = NumberUtil.defaultIfEmpty(parentLevel, this.LEVEL_BEGIN - 1) + 1;
-                const child = mapper ? mapper(children, obj, childList, level) : children;
-                this.toTreeMapper(list, child, idName, parentName, mapper, leafMapper, level);
+                const child = mapper ? mapper(children, obj, childList, level) : children
+                this.toTreeMapper(list, child, mapper, idName, parentName, leafMapper, level)
             }
         } else {
-            leafMapper && leafMapper(obj);
+            leafMapper && leafMapper(obj)
         }
     }
 }
